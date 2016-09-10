b06a212031ee817b218685a60e10ae6a0dcdea33
Kristopher Betz <kjbetz@gmail.com>
2016-09-10 16:06:39 -0400

    Branching

--- C#Path/01C#FunadamentalsWithVisualStudio2015/src/Grades/GradeStatistics.cs
+++ C#Path/01C#FunadamentalsWithVisualStudio2015/src/Grades/GradeStatistics.cs
@@ -8,6 +8,37 @@ namespace Grades
           LowestGrade = float.MaxValue;
         }
 
+        public string LetterGrade
+        {
+            get
+            {
+                string result;
+
+                if(AverageGrade >= 90)
+                {
+                    result = "A";
+                }
+                else if(AverageGrade >= 80)
+                {
+                    result = "B";
+                }
+                else if(AverageGrade >= 70)
+                {
+                    result = "C";
+                }
+                else if(AverageGrade >= 60)
+                {
+                    result = "D";
+                }
+                else
+                {
+                    result = "F";
+                }
+
+                return result;
+            }
+        }
+
         public float AverageGrade;
         public float HighestGrade;
         public float LowestGrade;
--- C#Path/01C#FunadamentalsWithVisualStudio2015/src/Grades/Program.cs
+++ C#Path/01C#FunadamentalsWithVisualStudio2015/src/Grades/Program.cs
@@ -8,11 +8,6 @@ namespace Grades
         {
             GradeBook book = new GradeBook();
 
-            book.NameChanged += OnNameChanged;
-
-            book.Name = "Scott's Grade Book";
-            book.Name = "Grade Book";
-
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
@@ -20,16 +15,12 @@ namespace Grades
             GradeStatistics stats = book.ComputeStatistics();
             Console.WriteLine(book.Name);
             WriteResult("Average", stats.AverageGrade);
-            WriteResult("Highest", (int)stats.HighestGrade);
+            WriteResult("Highest", stats.HighestGrade);
             WriteResult("Lowest", stats.LowestGrade);
+            WriteResult("Grade", stats.LetterGrade);
         }
 
-        static void OnNameChanged(object sender, NameChangedEventArgs args)
-        {
-            Console.WriteLine($"Grade book changing name from { args.ExistingName } to { args.NewName }");
-        }
-
-        static void WriteResult(string description, int result)
+        static void WriteResult(string description, string result)
         {
             Console.WriteLine($"{description}: {result}");
         }
